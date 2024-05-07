ee3235cf35fa9c709e203a3d9781fdb0690fc6b7
Anatoly Fedyanin <anfemail@gmail.com>
2024-05-07 20:09:37 +0300

    add files

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello!")
+## import
+import numpy as np
+import matplotlib.pyplot as plt
+
+## %% generate random data
+N = 100
+x = np.random.normal(0, 1, N)
+y = np.random.normal(2, 3, N)
+
+## plot data in histograms
+plt.hist(x, alpha=0.5, label='x')
+plt.hist(y, alpha=0.5, label='y')
+plt.legend()
+plt.show()
